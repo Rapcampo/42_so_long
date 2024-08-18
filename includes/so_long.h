eb0d967fac3af6a41780017ca86dadf74a659b5b
Raphael Campos Vieira <rapcampo@c2r2s6.42porto.com>
2024-08-18 20:26:18 +0100

    this is a good advance

--- includes/so_long.h
+++ includes/so_long.h
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -18,17 +19,96 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include "../minilibx/mlx.h" 
+# include <X11/X.h>
+# include <X11/keysym.h>
 
-# ifndef INT_MAX
-#  define INT_MAX 2147483647
-# endif
+// Keysyms for keypresses
 
-# ifndef INT_MIN
-#  define INT_MIN -2147483648 
-# endif
+# define KEY_A 97
+# define KEY_S 115
+# define KEY_D 100
+# define KEY_W 119
+# define KEY_ESC 65307
+# define KEY_UP 65362
+# define KEY_DOWN 65364
+# define KEY_LEFT 65361
+# define KEY_RIGHT 65363
 
-typedef struct s_list
+// defines for assets
+# define MAX_SPRITES 5
+# define ENTITIES "01CEP"
+# define BG_SPRITE "../assets/sprites/background.xpm"
+# define WALL_SPRITE "../assets/sprites/wall.xpm"
+# define PLAYER_SPRITE "../assets/sprites/player.xpm"
+# define COLLECT_SPRITE "../assets/sprites/collectable.xpm"
+# define EXIT_SPRITE "../assets/sprites/exit.xpm"
+
+// struct for mlx
+
+typedef struct s_window
 {
-	int
-}	t_list;
+	void	*mlx_ptr;
+	void	*win_ptr;
+	int		*height;
+	int		*width;
+	int		line_lenght;
+	int		bpp;
+	char	*addr;
+	int		endian;
+}	t_window;
+
+// struct for coordinates
+
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}	t_point;
+
+// struct for assets
+
+typedef struct s_assets
+{
+	char	**map;
+	void	*sprites;
+}	t_assets;
+
+// strct for gamestate
+
+typedef struct s_game
+{
+	t_window	win;
+	t_point		curr;
+	int			moves;
+	int			collect;
+}	t_game;
+
+// enum for map entities
+
+typedef enum e_entity
+{
+	FLOOR = '0',
+	WALL = '1',
+	POTION = 'C',
+	EXIT = 'E',
+	PLAYER = 'P'
+}	t_entity; 
+
+// enum for event hook mask
+// defined in x11/x.h 
+// close mask is for substructureNotifyMask for creation or destruction of win
+
+typedef enum e_mask
+{
+	NOEVENT_MASK	= 0L,
+	KEYPRESS_MASK	= (1L << 0),
+	SUBNOTE_MASK	= (1L << 17)
+}	t_mask;
+
+typedef enum e_event
+{
+	E_kEYPRESS	= 2,
+	E_DESTROY	= 17
+}	t_event;
+
 #endif
